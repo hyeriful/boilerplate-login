b37d12d7143ad539da15eed9537a578fd9776c22
hyeriful <dlgpfl2523@gmail.com>
2020-09-25 23:40:49 +0900

    navigation bar

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import LeftMenu from "./Sections/LeftMenu";
+import RightMenu from "./Sections/RightMenu";
+import { Drawer, Button } from "antd";
+import Icon from "@ant-design/icons";
+import "./Sections/Navbar.css";
 
 function NavBar() {
-    return (
-        <div>
-            NavBar
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <nav className="menu">
+      <div className="menu__container">
+        <div className="menu_left">
+          <LeftMenu mode="horizontal" />
         </div>
-    )
+        <div className="menu_right">
+          <RightMenu mode="horizontal" />
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
