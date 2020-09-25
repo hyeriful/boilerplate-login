95bf152c3a58a16498b35626c75e5138326a2ed2
hyeriful <dlgpfl2523@gmail.com>
2020-09-25 23:43:11 +0900

    update

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,31 +1,18 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import LandingPage from './views/LandingPage/LandingPage'
-import LoginPage from './views/LoginPage/LoginPage'
-import RegisterPage from './views/RegisterPage/RegisterPage'
+import LandingPage from "./views/LandingPage/LandingPage";
+import LoginPage from "./views/LoginPage/LoginPage";
+import RegisterPage from "./views/RegisterPage/RegisterPage";
 import Footer from "./views/Footer/Footer";
 import NavBar from "./views/NavBar/NavBar";
-import Auth from '../hoc/auth' //hoc안에 다른 컴포넌트를 넣어주는 방법
+import Auth from "../hoc/auth"; //hoc안에 다른 컴포넌트를 넣어주는 방법
 
 function App() {
   return (
     <Router>
       <NavBar />
       <div>
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
         <Switch>
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
